2febd5156e152e90671b0d8e70dfb4b796d9e70a
Vasilache Ionut <vasilacheorionut@gmail.com>
2022-05-14 11:03:49 +0200

    commit

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-#!/bin/sh
-
 i=0
 echo $i;
--- push.sh
+++ push.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
 let "i+=3";
 echo $i;
 git add * -f .gitignore 
-git commit -m "$push commit"
+git commit -m "$i commit"
 git push -u origin main 
